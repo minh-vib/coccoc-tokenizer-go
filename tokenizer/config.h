fd8deeb9490af360d2875983ad464792d5753f91
Pham Thai Hoa <thaihoabo@gmail.com>
2021-05-06 14:35:09 +0700

    Create config.h

---
+++ tokenizer/config.h
@@ -0,0 +1,12 @@
+#ifndef __TOKENIZER_CONFIG_H__
+#define __TOKENIZER_CONFIG_H__
+
+#ifndef DICT_PATH
+#define DICT_PATH "/usr/local/share/tokenizer/dicts"
+#endif
+
+#define MULTITERM_DICT_DUMP "multiterm_trie.dump"
+#define SYLLABLE_DICT_DUMP "syllable_trie.dump"
+#define NONTONE_PAIR_DICT_DUMP "nontone_pair_freq_map.dump"
+
+#endif /* __TOKENIZER_CONFIG_H__ */
