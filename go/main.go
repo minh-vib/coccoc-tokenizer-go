16361feafc210a328a497168028caea9cc193328
Hoang Minh <minh.hoang@vib.com.vn>
2023-10-30 23:27:05 +0700

    add stopwords filter

--- go/main.go
+++ go/main.go
@@ -7,13 +7,15 @@ import (
 )
 
 func main() {
-	token := tokenizer.NewTokenizer()
-	token.Options.SetDictPath("/usr/local/share/tokenizer/dicts")
+	tk := tokenizer.NewTokenizer()
+	//tk.Options.SetDictPath("/usr/local/share/tokenizer/dicts")
+	//tk.Options.SetStopWordType(tokenizer.STOP_WORD_CUSTOM)
+	tk.AddStopWords([]string{"Thegioididong", "vn"})
 
 	text := "toisongohanoi, tôi đăng ký trên Thegioididong.vn"
-	result := token.WordTokenizer(text)
+	result := tk.WordTokenizer(text)
 
 	fmt.Println(result)
 
-	token.Options.Destroy()
+	tk.Options.Destroy()
 }
